77b8cac13f3880557f2a66a35492a687bf520911
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2022-11-11 18:53:53 +0300

    Initial Commit

---
+++ PyIDE/PyIDEApp.swift
@@ -0,0 +1,17 @@
+//
+//  PyIDEApp.swift
+//  PyIDE
+//
+//  Created by Даниил Суханов on 11.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct PyIDEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
